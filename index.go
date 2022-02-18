aeecf5aa32a26695ca864987f6f933b6bf49338e
gjongenelen <g.jongenelen@pushto.space>
2022-02-18 09:18:28 +0100

    Add db-repo

---
+++ index.go
@@ -0,0 +1,91 @@
+package redipo
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/go-redis/redis/v8"
+	"github.com/google/uuid"
+)
+
+func (r *Repo) GetIndex(name string) ([]uuid.UUID, error) {
+	result, err := r.client.Get(context.Background(), r.name+"_"+name).Result()
+	if err != nil {
+		return nil, err
+	}
+
+	object := make([]uuid.UUID, 0)
+	err = json.Unmarshal([]byte(result), &object)
+	if err != nil {
+		return nil, err
+	}
+
+	return object, nil
+}
+
+func (r *Repo) ClearIndex(name string) error {
+	_, err := r.client.Del(context.Background(), r.name+"_"+name).Result()
+
+	return err
+}
+
+func (r *Repo) AddToIndex(name string, id uuid.UUID) error {
+	result, err := r.client.Get(context.Background(), r.name+"_"+name).Result()
+	if err != nil && err != redis.Nil {
+		return err
+	}
+
+	objects := make([]uuid.UUID, 0)
+	if err != redis.Nil {
+		err = json.Unmarshal([]byte(result), &objects)
+		if err != nil {
+			return err
+		}
+	}
+
+	for _, key := range objects {
+		if key == id {
+			return nil
+		}
+	}
+	objects = append(objects, id)
+	jsonVal, err := json.Marshal(objects)
+	if err != nil {
+		return err
+	}
+
+	_, err = r.client.Set(context.Background(), r.name+"_"+name, jsonVal, 0).Result()
+	return err
+}
+
+func (r *Repo) RemoveFromIndex(name string, id uuid.UUID) error {
+	result, err := r.client.Get(context.Background(), r.name+"_"+name).Result()
+	if err != nil && err != redis.Nil {
+		return err
+	}
+
+	objects := make([]uuid.UUID, 0)
+	if err != redis.Nil {
+		err = json.Unmarshal([]byte(result), &objects)
+		if err != nil {
+			return err
+		}
+	}
+
+	newObjects := make([]uuid.UUID, 0)
+	for _, key := range objects {
+		if key != id {
+			newObjects = append(newObjects, key)
+		}
+	}
+	if len(newObjects) == len(objects) {
+		return nil
+	}
+
+	jsonVal, err := json.Marshal(newObjects)
+	if err != nil {
+		return err
+	}
+
+	_, err = r.client.Set(context.Background(), r.name+"_"+name, jsonVal, 0).Result()
+	return err
+}
--- storage.go
+++ storage.go
@@ -2,7 +2,10 @@ package redipo
 
 import (
 	"context"
+	"fmt"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/go-redis/redis/v8"
@@ -12,6 +15,7 @@ import (
 type ManagerInterface interface {
 	ping() bool
 	LoadRepo(name string) RepoInterface
+	LoadDbRepo(name string) RepoInterface
 }
 
 type Manager struct {
@@ -27,21 +31,64 @@ func (m *Manager) LoadRepo(name string) RepoInterface {
 	return NewRepo(name, m.redis)
 }
 
-func New() ManagerInterface {
-	url := os.Getenv("REDIS")
-	manager := &Manager{
-		redis: redis.NewClient(&redis.Options{
-			Addr:     url,
-			Password: os.Getenv("password"),
-			DB:       0,
-		}),
+func (m *Manager) LoadDbRepo(name string) RepoInterface {
+	dbId := m.getDbNumber(name)
+
+	return NewRepo(name, newRedis(dbId))
+}
+
+func (m *Manager) getDbNumber(name string) int {
+	dbString, err := m.redis.Get(context.Background(), "databases").Result()
+	if err != nil {
+		dbString = ""
 	}
+	highest := 0
+	databases := strings.Split(dbString, ",")
+	for index, db := range databases {
+		dbParts := strings.Split(db, ":")
+		if len(dbParts) == 2 {
+			id, _ := strconv.Atoi(dbParts[1])
+			if highest < id {
+				highest = id
+			}
+
+			if dbParts[0] == name {
+				return id
+			}
+		} else {
+			databases = append(databases[:index], databases[index+1:]...)
+		}
+	}
+	databases = append(databases, fmt.Sprintf("%s:%d", name, highest+1))
+
+	_, err = m.redis.Set(context.Background(), "databases", strings.Join(databases, ","), 0).Result()
+
+	return highest + 1
+}
+
+func newRedis(db int) *redis.Client {
+	url := os.Getenv("REDIS_URL")
+	password := os.Getenv("REDIS_PASSWORD")
+
+	return redis.NewClient(&redis.Options{
+		Addr:     url,
+		Password: password,
+		DB:       db,
+	})
+}
+
+func New() ManagerInterface {
+
+	manager := &Manager{
+		redis: newRedis(0),
+	}
+
 	for i := 0; i < 10; i++ {
 		if manager.ping() {
-			log.Infof("Connected to Redis at %s", url)
+			log.Info("Connected to Redis")
 			return manager
 		} else {
-			log.Errorf("Could not connect to Redis at %s, retrying...", url)
+			log.Error("Could not connect to Redis, retrying...")
 			time.Sleep(1 * time.Second)
 		}
 	}
